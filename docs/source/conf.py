49b9b11d70ec26563e1a15bb51eea01e5e162487
Ruihua <easymath00@gmail.com>
2025-11-06 22:04:30 +0100

    add code to keep scan root at the top of ndif_ray so AutoAPI can still resolve intra-package imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,4 +16,11 @@ autoapi_root = "reference/api"
 autoapi_keep_files = True
 
 
+PROJECT_ROOT = os.path.abspath(os.path.join(__file__, "..", "..", ".."))
+PKG_PARENT   = os.path.join(PROJECT_ROOT, "src", "services", "ray", "src")
+sys.path.insert(0, PKG_PARENT)
+
+autoapi_type = "python"
+autoapi_dirs = [os.path.join(PKG_PARENT, "ndif_ray")]   # scan full package
+autoapi_root = "reference/api"
 
